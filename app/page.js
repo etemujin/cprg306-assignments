8131a2782d23da2d990d10475ecd51d88d4fcdf3
Ethan Bayarsaikhan <165347712+etemujin@users.noreply.github.com>
2026-01-20 14:34:03 -0700

    week 2 assignment

---
+++ app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+
+      <p>
+        <a href="/week-2">Week 2</a>
+      </p>
+    </main>
+  );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Name: Ethan Bayarsaikhan</p>
+      <p>
+        GitHub:{" "}
+        <a href="https://github.com/etemujin/cprg306-assignments"  style={{ textDecoration: "underline" }}>
+          etemujin/cprg306-assignments
+        </a>
+      </p>
+    </div>
+  );
+}
